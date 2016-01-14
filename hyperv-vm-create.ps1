75af28cd5ea3c37dea55b29c80e476c2647aa4ae
keawade <keawade@gmail.com>
2016-01-14 15:25:13 -0600

    Base script for hyper-v machines created

--- hyperv-vm-create.ps1
+++ hyperv-vm-create.ps1
@@ -1,1 +1,45 @@
-# This is a comment!
+#------------------------------------------------------------------------------#
+# Purpose:                                                                     #
+#   Create multiple virtual machines with differencing disks                   #
+# Contributors:                                                                #
+#   Shane Flowers                                                              #
+#   Keith Wade                                                                 #
+#------------------------------------------------------------------------------#
+
+$machine_list = Get-Content ""
+
+for($i=0; $i -le 1; $i++){
+  $vmname = $machine_list[$i]
+  $location = "E:\VMs\$vmname"
+  
+  New-VHD -Path "" `
+    -Differencing `
+    -ParentPath ""
+
+  New-VM -Name `
+    -MemoryStartupBytes 2GB `
+    -Generation 2 `
+    -Path "" `
+    -SwitchName "" `
+    -VHDPath ""
+
+  Set-VM -Name `
+    -DynamicMemory `
+    -MemoryMaximumBytes 8GB `
+    -MemoryMinimumBytes 2GB `
+    -MemoryStartupBytes 2GB `
+    -ProcessorCount 4 `
+    -SmartPagingFilePath "" `
+    -SnapshotFileLocation "" `
+    -Notes ""
+
+  Set-VMFirmware $vmname `
+    -EnableSecureBoot off
+  $adapter = Get-VMNetworkAdapter `
+    -VM $vmname `
+    -Name *
+
+  Set-VMNetworkAdapterVlan `
+    -VMNetworkAdapter $adapter `
+    -VlanId
+}
